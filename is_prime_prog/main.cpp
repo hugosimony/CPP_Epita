e8a22d7bbfe3b81aadd8b5eea64128325ee6fe24
Hugo Simony <hugosimonyjungo@aol.fr>
2022-10-19 23:35:59 +0200

    Finished is_prime_prog

--- is_prime_prog/main.cpp
+++ is_prime_prog/main.cpp
@@ -3,28 +3,30 @@
 
 int main()
 {
-    int x;
-    while (std::cix >> x)
+    long long i, n;
+
+    while (std::cin >> n)
     {
-        if (is_prime(x))
-            std::cout << x << " is a prime: True\n";
+        bool is_prime = true;
+
+        if (n <= 1)
+        {
+            is_prime = false;
+        }
+
+        for (i = 2; i <= std::sqrt(n); ++i)
+        {
+            if (n % i == 0)
+            {
+                is_prime = false;
+                break;
+            }
+        }
+
+        if (is_prime)
+            std::cout << n << " is a prime: True\n";
         else
-            std::cout << x << " is a prime: False\n";
+            std::cout << n << " is a prime: False\n";
     }
     return 0;
-}
-
-bool is_prime(int n) {
-    
-    if (n == 2 || n == 3)
-        return true;
-    
-    if (n <= 1 || n % 2 == 0 || n % 3 == 0)
-        return false;
-
-    for (int i = 5; i * i <= n; i += 6)
-        if (n % i == 0 || n % (i + 2) == 0)
-            return false;
-
-    return true;
 }
