5593b7ee6a8710e20e2bf755c72d94c0bac431a9
Hugo Simony <hugosimonyjungo@aol.fr>
2022-11-17 16:44:28 +0100

    First version selection_sort

---
+++ selection_sort/my_sort.hpp
@@ -0,0 +1,9 @@
+// Change me instead
+// All occurences of XXX need to be replaced with
+// something more meaningful
+
+template <typename T>
+void swap(T *xp, T *yp);
+
+template <typename T>
+void my_selection_sort( T begin, T end);
