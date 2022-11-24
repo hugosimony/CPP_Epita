1c303c7fe2ae0b1ec448e02195dd1ae384062a63
Hugo Simony-Jungo <hugo.simony-jungo@epita.fr>
2022-11-24 09:03:30 -0500

    Fixed selection_sort and almost finished selection_sort_file

--- selection_sort_file/sort_file.hpp
+++ selection_sort_file/sort_file.hpp
@@ -14,32 +14,43 @@
 // something more meaningful
 
 
-XXX
-void read_file_to_cont(XXX& f, XXX c) {
-  XXX
+template <typename T>
+void read_file_to_cont(std::ifstream &fi, std::list<T> &cont) {
+    T val;
+    while (fi >> val)
+        cont.push_back(val);
 }
 
-XXX
-void read_and_sort(XXX& fi, XXX& fo) {
-  XXX
+template <typename U>
+void read_and_sort(std::ifstream &fi, std::ofstream &fo) {
+  std::list<U> cont;
+  read_file_to_cont(fi, cont);
+  my_selection_sort(cont.begin(), cont.end());
+  for (auto &val : cont)
+    fo << val << std::endl;
 }
 
-XXX
-void read_and_sort_decide_container(XXX& fi, XXX& fo) {
-  XXX
-  switch(XXX)
-  {
-    XXX
+template <typename V>
+void read_and_sort_decide_container(std::ifstream &fi, std::ofstream &fo) {
+  if (fi.peek() == EOF)
+    return;
+  V val;
+  fi >> val;
+  if (fi.peek() == EOF) {
+    fo << val << std::endl;
+    return;
   }
+  fi.seekg(0);
+  read_and_sort<V>(fi, fo);
 }
 
-void read_and_sort_decide_valuetype(XXX& fi, XXX& fo) {
-  XXX
-  switch(XXX)
-  {
-    XXX
-  }
+void read_and_sort_decide_valuetype(std::ifstream &fi, std::ofstream &fo) {
+  if (fi.peek() == EOF)
+    return;
+  char c = fi.peek();
+  if (c == '-' || (c >= '0' && c <= '9'))
+    read_and_sort_decide_container<int>(fi, fo);
+  else
+    read_and_sort_decide_container<std::string>(fi, fo);
 }
-
-
 
