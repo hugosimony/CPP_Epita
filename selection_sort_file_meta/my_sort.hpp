281b38147a8cf73e6147721eaf36cff4c9080a74
Hugo Simony <hugosimonyjungo@aol.fr>
2022-12-13 16:12:09 +0100

    Fixed the sorting

--- selection_sort_file_meta/my_sort.hpp
+++ selection_sort_file_meta/my_sort.hpp
@@ -2,16 +2,39 @@
 // All occurences of XXX need to be replaced with
 // something more meaningful
 
-template<typename iterator>
+template <typename iterator>
 void my_selection_sort(iterator begin, iterator end) {
-    for (auto i = begin; i != end; ++i) {
-        auto min = i;
-        for (auto j = i; j != end; ++j) {
-            if (*j < *min)
-                min = j;
+    typename std::remove_pointer<typename iterator::value_type>::type a;
+
+    if constexpr (std::is_integral<typeof(a)>::value) {
+        size_t evens = 0;
+        iterator odds = begin;
+        for (iterator i = begin; i != end; i++) {
+            if (*i % 2 == 0) {
+                std::swap(*i, *odds++);
+                evens++;
+            }
         }
-        auto tmp = *i;
-        *i = *min;
-        *min = tmp;
-    }
+        for (iterator j = begin; j != odds; j++) {
+            iterator min = j;
+            for (iterator it = ++iterator(j); it != odds; it++)
+                if (*it < *min) min = it;
+
+            std::swap(*j, *min);
+        }
+        for (iterator j = odds; j != end; j++) {
+            iterator min = j;
+            for (iterator it = ++iterator(j); it != end; it++)
+                if (*it < *min) min = it;
+
+            std::swap(*j, *min);
+        }
+    } else
+        while (begin != end) {
+            iterator min = begin;
+            for (iterator it = ++iterator(begin); it != end; it++)
+                if (*it < *min) min = it;
+
+            std::swap(*(begin++), *min);
+        }
 }
